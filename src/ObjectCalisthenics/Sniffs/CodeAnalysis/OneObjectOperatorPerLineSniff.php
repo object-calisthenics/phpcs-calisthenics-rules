6a1bc9d5c07b71a673962d25d706a1e5e5d156cd
Oskar Pfeifer-Bley <pfeifer-bley@ipa-dguv.de>
2017-02-22 10:45:01 +0100

    Exclude fluent interfaces from OneObjectOperatorPerLine

--- src/ObjectCalisthenics/Sniffs/CodeAnalysis/OneObjectOperatorPerLineSniff.php
+++ src/ObjectCalisthenics/Sniffs/CodeAnalysis/OneObjectOperatorPerLineSniff.php
@@ -27,6 +27,32 @@ final class OneObjectOperatorPerLineSniff implements PHP_CodeSniffer_Sniff
      */
     private $callerTokens;
 
+    /**
+     * @var string
+     */
+    private $variableName;
+
+    /**
+     * Comma separated list of variables that will start the fluent interface mode.
+     *
+     * @var string
+     */
+    public $variablesHoldingAFluentInterface = '$queryBuilder';
+
+    /**
+     * Comma separated list of methods that will start the fluent interface mode.
+     *
+     * @var string
+     */
+    public $methodsStartingAFluentInterface = 'createQueryBuilder';
+
+    /**
+     * Comma separated list of methods that will end the fluent interface mode.
+     *
+     * @var string
+     */
+    public $methodsEndingAFluentInterface = 'execute,getQuery';
+
     public function register() : array
     {
         return [T_VARIABLE];
@@ -43,6 +69,7 @@ final class OneObjectOperatorPerLineSniff implements PHP_CodeSniffer_Sniff
         $this->callerTokens = [];
 
         $tokens = $phpcsFile->getTokens();
+        $this->variableName = $tokens[$stackPtr]['content'];
         $pointer = $this->ignoreWhitespace($tokens, $stackPtr + 1);
 
         $token = $tokens[$stackPtr];
@@ -68,7 +95,7 @@ final class OneObjectOperatorPerLineSniff implements PHP_CodeSniffer_Sniff
 
     private function handleTwoObjectOperators(bool $isOwnCall)
     {
-        if ($this->callerTokens && !$isOwnCall) {
+        if ($this->callerTokens && !$isOwnCall && !$this->isInFluentInterfaceMode()) {
             $this->phpcsFile->addError('Only one object operator per line.', $this->stackPtr);
 
             throw new \Exception();
@@ -88,12 +115,44 @@ final class OneObjectOperatorPerLineSniff implements PHP_CodeSniffer_Sniff
         if (
             ($memberTokenType === 'property' && $tmpTokenType === 'property') ||
             ($memberTokenType === 'method' && $tmpTokenType === 'property') ||
-            ($memberTokenType === 'method' && $tmpTokenType === 'method' && $memberTokenCount > 1 && $memberToken['token']['content'] !== $tmpToken['content'])
+            ($memberTokenType === 'method' && $tmpTokenType === 'method'
+                && $memberTokenCount > 1 && $memberToken['token']['content'] !== $tmpToken['content'] && !$this->isInFluentInterfaceMode())
         ) {
             $this->phpcsFile->addError('Only one object operator per line.', $this->stackPtr);
 
             throw new \Exception();
         }
+    }
+
+    private function isInFluentInterfaceMode(): bool
+    {
+        $lastEndPoint = $this->computeLastCallOfAnyFrom(explode(',', $this->methodsEndingAFluentInterface));
+        $lastStartPoint = $this->computeLastCallOfAnyFrom(explode(',', $this->methodsStartingAFluentInterface));
+
+        if (in_array($this->variableName, explode(',', $this->variablesHoldingAFluentInterface), true)) {
+            $lastStartPoint = max($lastStartPoint, -1);
+        }
+
+        return $lastStartPoint > -2
+            && $lastStartPoint > $lastEndPoint;
+    }
+
+    /**
+     * @param array $methods
+     *
+     * @return int The last position of the method calls within the callerTokens
+     *             or -2 if none of the methods has been called
+     */
+    private function computeLastCallOfAnyFrom(array $methods): int
+    {
+        $calls = array_filter($this->callerTokens, function (array $token) use ($methods) {
+            return in_array($token['token']['content'], $methods);
+        });
+        if (count($calls) > 0) {
+            return array_search(end($calls), $this->callerTokens);
+        }
+
+        return -2;
     }
 
     private function handleObjectOperators(array $tokens, int $pointer, bool $isOwnCall)
