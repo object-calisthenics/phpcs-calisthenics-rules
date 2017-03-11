473948bf9c4161b9c206fbda3a081ee42a267237
TomasVotruba <tomas.vot@gmail.com>
2017-03-11 11:29:18 +0100

    tests: add test for ClassAnalyzer

--- src/ObjectCalisthenics/Helper/ClassAnalyzer.php
+++ src/ObjectCalisthenics/Helper/ClassAnalyzer.php
@@ -2,6 +2,7 @@
 
 namespace ObjectCalisthenics\Helper;
 
+use Exception;
 use ObjectCalisthenics\Helper\DocBlock\MemberComment;
 use PHP_CodeSniffer\Files\File;
 
@@ -14,6 +15,8 @@ final class ClassAnalyzer
 
     public static function getClassMethodCount(File $file, int $position): int
     {
+        self::ensureIsClassTraitOrInterface($file, $position);
+
         $methodCount = 0;
         $pointer = $position;
 
@@ -51,15 +54,29 @@ final class ClassAnalyzer
         $tokens = $file->getTokens();
         $property = $tokens[$position];
 
+
         // Is it a property or a random variable?
         if (!(count($property['conditions']) === 1 && in_array(reset($property['conditions']), [T_CLASS, T_TRAIT]))) {
             return;
         }
 
-        if ($comment = MemberComment::getMemberComment($file, $position)) {
-            self::$propertyList[] = [
-                'type' => $comment,
-            ];
+        $comment = MemberComment::getMemberComment($file, $position);
+        self::$propertyList[] = [
+            'type' => $comment ?: 'unknown',
+        ];
+    }
+
+    private static function ensureIsClassTraitOrInterface(File $file, int $position): void
+    {
+        $token = $file->getTokens()[$position];
+
+        if ( ! in_array($token['code'], [T_CLASS, T_INTERFACE, T_TRAIT])) {
+            throw new Exception(
+                sprintf(
+                    'Must be class, interface or trait. "%s" given.',
+                    ltrim($token['type'], 'T_')
+                )
+            );
         }
     }
 }
---
+++ tests/Helper/ClassAnalyzerTest.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types = 1);
+
+namespace ObjectCalisthenics\Tests\Helper;
+
+use ObjectCalisthenics\Helper\ClassAnalyzer;
+use ObjectCalisthenics\Tests\FileFactory;
+use PHP_CodeSniffer\Files\File;
+use PHPUnit\Framework\TestCase;
+
+final class ClassAnalyzerTest extends TestCase
+{
+    /**
+     * @var int
+     */
+    private const CLASS_POSITION = 3;
+
+    /**
+     * @var FileFactory
+     */
+    private $fileFactory;
+
+    /**
+     * @var File
+     */
+    private $file;
+
+    protected function setUp()
+    {
+        $fileFactory = new FileFactory;
+        $this->file = $fileFactory->createFile(__DIR__ . '/ClassAnalyzerSource/SomeFile.php.inc');
+    }
+
+    public function testMethodCount()
+    {
+        $this->assertSame(2, ClassAnalyzer::getClassMethodCount($this->file, self::CLASS_POSITION));
+    }
+
+    public function testProperties()
+    {
+        $properties = ClassAnalyzer::getClassProperties($this->file, self::CLASS_POSITION);
+
+        $this->assertCount(2, $properties);
+
+        $this->assertArrayHasKey(1, $properties);
+        $this->assertArrayHasKey('type', $properties[1]);
+        $this->assertSame('int', $properties[1]['type']);
+    }
+
+    public function testPropertyCount()
+    {
+        $this->assertSame(2, ClassAnalyzer::getClassPropertiesCount($this->file, self::CLASS_POSITION));
+    }
+}
