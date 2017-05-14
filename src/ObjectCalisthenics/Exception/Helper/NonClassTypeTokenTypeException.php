b5e978cf80d54c35ccd538fb03b1617b9fc03f62
TomasVotruba <tomas.vot@gmail.com>
2017-05-14 17:54:57 +0200

    improve exception

--- src/ObjectCalisthenics/Exception/Helper/NonClassTypeTokenTypeException.php
+++ src/ObjectCalisthenics/Exception/Helper/NonClassTypeTokenTypeException.php
@@ -1,7 +1,10 @@
 <?php declare(strict_types=1);
 
+namespace ObjectCalisthenics\Exception\Helper;
 
-class NonClassTypeTokenTypeException
+use Exception;
+
+final class NonClassTypeTokenTypeException extends Exception
 {
 
 }
