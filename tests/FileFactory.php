bf2d388baa4262bb979f8bd05529047e4b42a4c2
TomasVotruba <tomas.vot@gmail.com>
2017-06-17 10:34:34 +0200

    details

--- tests/FileFactory.php
+++ tests/FileFactory.php
@@ -20,7 +20,7 @@ final class FileFactory
 
         // initialize Token constants
         if (! defined('T_NONE')) {
-            new Tokens();
+            new Tokens;
         }
     }
 
@@ -61,7 +61,7 @@ final class FileFactory
 
     private function createConfig(): Config
     {
-        $config = new Config();
+        $config = new Config;
         // nulling required, because PEAR Standard is on by default
 //        $config->standards = [];
 
