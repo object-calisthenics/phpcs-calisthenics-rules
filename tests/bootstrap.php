aac08583d5519408b12c37496e29dfafdad05001
TomasVotruba <tomas.vot@gmail.com>
2017-06-17 10:12:05 +0200

    fix tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,9 +2,3 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/squizlabs/php_codesniffer/autoload.php';
-
-//new PHP_CodeSniffer\Util\Tokens;
-//
-//if (! defined('PHP_CODESNIFFER_VERBOSITY')) {
-//    define('PHP_CODESNIFFER_VERBOSITY', 0);
-//}
