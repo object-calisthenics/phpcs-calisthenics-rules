31a3a6d670aa6a229047ce5526a785318c6b7701
TomasVotruba <tomas.vot@gmail.com>
2019-11-10 14:06:36 +0100

    enable dead-code set

--- src/ObjectCalisthenics/Sniffs/Metrics/MaxNestingLevelSniff.php
+++ src/ObjectCalisthenics/Sniffs/Metrics/MaxNestingLevelSniff.php
@@ -79,8 +79,6 @@ final class MaxNestingLevelSniff implements Sniff
      */
     private function iterateTokens(int $start, int $end, array $tokens): void
     {
-        $this->currentPtr = $start + 1;
-
         // Find the maximum nesting level of any token in the function.
         for ($this->currentPtr = ($start + 1); $this->currentPtr < $end; ++$this->currentPtr) {
             $nestedToken = $tokens[$this->currentPtr];
